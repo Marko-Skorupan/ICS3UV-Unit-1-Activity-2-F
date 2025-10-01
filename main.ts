9e75b5dfb4cb94837f85aa87303f21253ecb48fe
default <marko.skorupan@stu.ocsb.ca>
2025-10-01 19:18:48 +0000

    Made the program calculate my year born and year graduating using sum, difference, product, quotient, and modulo

---
+++ main.ts
@@ -0,0 +1,23 @@
+/**
+ * @author Marko Skorupan
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview "Displays Happy Birthday song with my name."
+ */
+
+// Headings fot the program
+console.log("Year Born  Year Graduating  Result")
+
+
+//All calculations for sum, difference, product, quotient, and modulo
+console.log("   2009   +     2027       = " +String(2009+2027));
+
+console.log("   2009   -    2027        = " + String(2009-2027));
+
+console.log("   2009   *    2027        = " + String(2009*2027));
+
+console.log("   2009   /    2027        = " + String(2009/2027));
+
+console.log("   2009   %    2027        = " + String(2009%2027));
+
+console.log("\nDone");
